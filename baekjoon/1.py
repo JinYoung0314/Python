ae8674472860f545c69501caa3f79da72b642ec0
JinYoung0314 <Jangyoungjin03@gmail.com>
2025-06-19 15:53:32 +0900

    study

---
+++ baekjoon/1.py
@@ -0,0 +1,27 @@
+# 간단한 수학 연산
+
+# 백준 1번
+# 첫째 줄에 A와 B 입력 받은 다음, A+B를 출력하기
+a, b = map(int, input().split())
+print(a + b)
+
+# 백준 2번
+# 첫째 줄에 A와 B 입력 받은 다음, A-B를 출력하기
+a, b = map(int, input().split())
+print(a - b)
+
+# 백준 3번
+# 첫째 줄에 A와 B 입력 받은 다음, A/B를 출력하기
+a, b = map(int, input().split())
+print(a / b)
+
+# # 백준 4번
+# # 첫째 줄에 A와 B를 입력 받은 다음, A*B를 출력하기
+a, b = map(int, input().split())
+print(a * b)
+
+# 백준 1번
+# A와 B 입력 받은 다음, A + B를 출력하기
+a = int(input("a를 입력하세요: "))
+b = int(input("b를 입력하세요: "))
+print(a + b)
