40da15268b978c11e3303c66e699c52839314ad6
JinYoung0314 <Jangyoungjin03@gmail.com>
2025-07-15 15:08:45 +0900

    code up

---
+++ codeup/c2.py
@@ -0,0 +1,75 @@
+# # 문자 한개 입력 받아 a 출력하기
+
+# p = input()
+# print(p)
+
+# # 변수를 정수 값으로 저장 후 변환하여 15 출력하기
+
+# p = int(input())
+# print(p)
+
+# # 변수를 실수 값으로 저장 후 변환하여 1.414213 출력하기
+
+# p = float(input())
+# print(p)
+
+# # 두 개의 정수를 입력 받아 그대로 출력하기
+
+# p = int(input())
+# a = int(input())
+
+# print(p)
+# print(a)
+
+# # 두 개의 문자를 입력 받아 순서 바꿔 출력하기
+
+# p = input()
+# a = input()
+
+# print(a)
+# print(p)
+ 
+# # 실수 한 개를 입력 받아 3번 출력하기
+
+# p = float(input())
+
+# print(p)
+# print(p)
+# print(p)
+
+# # 정수 2개를 입력 받아 그대로 출력하기
+
+# # p = int(input())
+# # a = int(input())
+
+# # print(p)
+# # print(a)
+
+# p,a = input().split()
+
+# p = int(p)
+# a = int(a)
+
+# print(p)
+# print(a)
+
+# # 문자 2개를 입력 받아 순서 바꾸어 출력하기
+
+# p,a = input().split()
+
+# p = str(p)
+# a = str(a)
+
+# print(a, p)     # *순서 바꾸어 출력한다는 문제를 잘 숙지하기*
+
+# # 문장 1개를 입력 받아 3번 출력하기
+
+# p = input()
+
+# print(p, p, p)
+
+# 시간을 입력 받아 그대로 출력하기
+
+p,a = input().split(':')
+print(p, a, sep=':') # sep="" -> 분류 기호 
+
