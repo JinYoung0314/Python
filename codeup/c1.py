0b0502ff7c11b8abd095bfb1bf87b1e31f0f551e
JinYoung0314 <Jangyoungjin03@gmail.com>
2025-07-17 17:42:03 +0900

    codeup

--- codeup/c1.py
+++ codeup/c1.py
@@ -1,32 +1,32 @@
-# print() => Hello 출력하기
+# 1.print() => Hello 출력하기
 
 print("Hello")
 
-# print() = > Hello World 출력하기
+# 2.print() = > Hello World 출력하기
 
 print("Hello World")
 
-# print() => Hello / World를 2줄로 나누어서 출력하기
+# 3.print() => Hello / World를 2줄로 나누어서 출력하기
 
 print("Hello")
 print("World")
 
-# print() => 'Hello' 출력하기
+# 4.print() => 'Hello' 출력하기
 
 print("'Hello'")
 
-# print() => "Hello World" 출력하기
+# 5.print() => "Hello World" 출력하기
 
 print('"Hello World"')
 
-# print() => "!@#$%^&*()' 출력하기
+# 6.print() => "!@#$%^&*()' 출력하기
 
 print('\"!@#$%^&*()\'') # \'  \"를 사용하여 푸니까 되더라! \" -> " , \' -> ' 로 출력됨!
 
-# print() => "C:\Download\'hello'.py" 출력하기
+# 7.print() => "C:\Download\'hello'.py" 출력하기
 
 print('\"C:\Download\\\'hello\'.py\"')
 
-# print() => print("Hello\nWorld") 출력하기
+# 8.print() => print("Hello\nWorld") 출력하기
 
 print('print("Hello\\nWorld")')
---
+++ codeup/c3.py
@@ -0,0 +1,32 @@
+# # 19.연월일 입력 받아 순서 바꿔 출력하기 
+
+# y, m, d = input().split('.')
+
+# y = int(y)
+# m = int(m)
+# d= int(d)
+
+# print(d, m, y,sep='-')
+
+
+# # 20.주민번호 입력 받아 형태 바꿔 출력하기
+
+# a, b = input().split("-")
+
+# print(a, b, sep="")
+
+# # 21.단어 1개 입력 받아 나누어 출력하기
+
+# a = input()
+
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+
+# # 22. 연월일 입력 받아 나누어 출력하기 
+
+# s = input()
+# print(s[0:2], s[2:4], s[4:6], end="")
+0
