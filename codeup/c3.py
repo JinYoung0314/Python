b46f53d1c9452384a2cae6b8f96b708d3f4e91ed
JinYoung0314 <Jangyoungjin03@gmail.com>
2025-07-18 17:01:32 +0900

    code up

--- codeup/c3.py
+++ codeup/c3.py
@@ -7,6 +7,10 @@
 # d= int(d)
 
 # print(d, m, y,sep='-')
+# --------------------------
+# y, m, d = map(int, input().split(','))
+
+# print(d, m, y, sep='-')
 
 
 # # 20.주민번호 입력 받아 형태 바꿔 출력하기
@@ -30,3 +34,36 @@
 # s = input()
 # print(s[0:2], s[2:4], s[4:6], end="")
 
+# # 23. 시분초 입력 받아 분만 출력하기
+
+# t, m, s= input().split(':')
+# print(m)
+
+# # 24. 단어 2개 입력 받아 이어 붙이기
+
+# word, word2 = input().split(' ')
+
+# t = word + word2
+# print(t)
+
+# # 25. 정수 2개 입력 받아 합 계산하기
+
+# a, b = input().split(' ')
+
+# a= int(a)
+# b= int(b)
+
+# c = a + b
+
+# print(c)
+
+# # 26. 실수 2개 입력 받아 합 계산하기
+
+# a = input()
+# b = input()
+
+# a= float(a)
+# b= float(b)
+# c= a + b
+
+# print(c)
--- codeup/tempCodeRunnerFile.py
+++ codeup/tempCodeRunnerFile.py
@@ -1,7 +1,4 @@
-# 14.실수 한 개를 입력 받아 3번 출력하기
-
-# p = float(input())
-
-# print(p)
-# print(p)
-# print(p)
+, w1 = map(int, input().split())
+# print(w - w1)
+ 
+ 
