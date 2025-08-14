163f756daacf734c92baae648af7390779f1d7fd
JinYoung0314 <Jangyoungjin03@gmail.com>
2025-08-14 17:51:43 +0900

    codeup

--- codeup/c6.py
+++ codeup/c6.py
@@ -11,15 +11,39 @@
 
 # 72. 정수 1개 입력 받아 카운트 다운 출력하기
 
-a = 3
+# a = int(input())
 
-while a != 0:
-    a = int(input())
-    if a != 0:
-        if a != 0:
-            a -= 1
-            print(a)
+# while a > 0:
+#     print(a)    
+#     a -= 1
+        
+# 73. 정수 1개 입력 받아 카운트 다운 출력하기.
+
+# a = int(input())
+
+# while a > 0:
+#     a -= 1
+#     print(a)
+
+# 74. 문자 1개 입력 받아 알파벳 출력하기
+
+# a = ord(input())
+# b = ord('a')
+# while b <= a:
+#     print(chr(b), end=' ')
+#     b += 1
+
+# 75. 정수 1개 입력 받아 그 수까지 출력하기.
+
+# a = int(input())
+
+# b = 0
+
+# while a >= b:
+#     print(b)
+#     b += 1
     
-    if a == 0:
-        break
     
+# 76. 정수 1개 입력 받아 그 수까지 출력하기
+
+a = int(input())
